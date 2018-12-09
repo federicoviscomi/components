48c8aa15518799ca6f901a8e99ad8d484f81b8f9
pako <pako@Federicos-MBP.lan>
2018-12-09 12:45:57 +0000

    merge

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import CommentDetail from "./CommentDetail";
 import faker from 'faker';
 import ApprovalCard from "./ApprovalCard";
+import React, {Component} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import faker from "fakswer";
 
 class App extends Component {
     author = 'asdf author';
@@ -16,6 +20,24 @@ class App extends Component {
                 </div>
                 <div>
                     <ApprovalCard/>
+                </div>
+                <div>
+                    <div className="ui container comments">
+                        <div className="comment">
+                            <a href="/" className="avatar">
+                                <img alt="avatar" src={faker.image.avatar()}/>
+                            </a>
+                            <div className="content">
+                                <a href="/" className="author">
+                                    Sam
+                                </a>
+                                <div>
+                                    <span className="date">Today at 6:00PM</span>
+                                </div>
+                                <div className="text">Nice blog post!</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
